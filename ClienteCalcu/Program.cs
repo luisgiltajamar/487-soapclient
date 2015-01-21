e866a5a08c83d16fac55670e861ad01cebf22457
lamaslg <lamaslg@gmail.com>
2015-01-21 10:18:05 +0100

    a

---
+++ ClienteCalcu/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ClienteCalcu.ServiceReference1;
+
+namespace ClienteCalcu
+{
+    class Program
+    {
+      
+       public static void Main(string[] args)
+       {
+           double[] o = {3, 6, 9, 5, 4};
+
+           var cl = new CalculadoraClient();
+
+           var r = cl.Sumar(2, 2);
+
+           Console.WriteLine(r);
+
+           var rr = cl.SumaCompleja(o);
+
+           Console.WriteLine(rr.Resultado + "->" + rr.Operandos);
+
+           var d = 0d;
+
+           try
+           {
+               d= cl.Dividir(2, 0);
+           }
+           catch (FaultException<ErrorDivision> e)
+           {
+               Console.WriteLine(e.Detail.Descripcion);
+               Console.WriteLine(e.Message);
+           }
+
+           Console.WriteLine(d);
+
+
+           Console.ReadLine();
+
+
+       }
+    }
+}
